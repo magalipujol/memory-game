1c35c04b106dc15d80326c4c6690e7fb4ccc4266
magalipujol <magalipuuujol@gmail.com>
2021-05-06 13:40:20 -0300

    almost done :(

--- main.js
+++ main.js
@@ -22,11 +22,6 @@ function getRandomNumberUpToANumber(biggest) {
 let notDupArr = ["first", "second", "third", "fourth", "fifth", "sixth"]
 let dupArr = ["first-dup", "second-dup", "third-dup", "fourth-dup", "fifth-dup", "sixth-dup"]
 
-function iterateArr (arr) {
-    for (let x of array) {
-        f(x)
-    }
-}
 notDupArr.forEach(
 x => document.getElementById(x).src = cardsArr[getRandomNumberUpToANumber(cardsArr.length - 1)]
 )
@@ -34,3 +29,74 @@ x => document.getElementById(x).src = cardsArr[getRandomNumberUpToANumber(cardsA
 notDupArr.forEach( x =>
  document.getElementById(x + "-dup").src = document.getElementById(x).src 
 )
+
+//this selects all memory-card elments
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false
+let lockBoard = false;
+let firstCard, secondCard;
+
+//the "this" variable represents the card that was clicked
+//this function accesses the element's classList and activate the flip class
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+    this.classList.add('flip');
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+    secondCard = this;
+    checkForMatch();
+}
+
+function checkForMatch() {
+    //TODO
+ if (firstCard.innerHTML === secondCard.innerHTML) {
+
+    disableCards();
+ 
+    return;
+ 
+  }
+ 
+ 
+  unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard();
+}
+
+function unflipCards() {
+     lockBoard = true;
+     setTimeout(() => {
+         firstCard.classList.remove('flip');
+         secondCard.classList.remove('flip');
+         resetBoard()
+        }, 1500);
+    }
+    
+    
+    function resetBoard() {
+        [hasFlippedCard, lockBoard] = [false, false];
+        [firstCard, secondCard] = [null, null];
+    }
+    
+    
+    
+    (function shuffle() {
+        cards.forEach(card => {
+            let ramdomPos = Math.floor(Math.random() * 12);
+            card.style.order = ramdomPos;
+        });
+        
+    })();
+    
+    //this makes that every time a card gets clicked flipCard function will be fired
+    cards.forEach(card => card.addEventListener('click', flipCard))
+
